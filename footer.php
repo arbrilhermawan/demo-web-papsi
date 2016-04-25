698a400a821ad0d740ab87acce5d0e396a207830
arbrilhermawan <faridzarbrilian@gmail.com>
2016-04-25 20:49:35 +0700

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!--start-footer-section-->
+			<div class="footer-section">
+						<div class="container">
+							<div class="footer-top">
+						<p>&copy; 2016 <span>Arbrilhermawan.</span> All rights reserved</p>
+									</div>
+							
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+				</div>
+			</div>
+	<!--end-footer-section-->
+    
+    <!-- Go To Top Link -->
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  
+  <script type="text/javascript" src="../js/script.js"></script>
+<script src="../js/jquery-1.11.1.min.js"></script>
+<!---- start-smoth-scrolling---->
+<script type="text/javascript" src="../js/move-top.js"></script>
+<script type="text/javascript" src="../js/easing.js"></script>
+<!---End-smoth-scrolling---->
+ 
+
+</body>
+</html> 
---
+++ header.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title><?php echo $title ?></title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<link href="css/slicknav.css" rel="stylesheet" type="text/css">
+<link href="css/style (2).css" rel="stylesheet" type="text/css">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+
+ 
+</head>
+<body>
