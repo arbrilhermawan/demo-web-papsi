036b4bfab35c5c6159cb07bba8be058428a71a5c
arbrilhermawan <faridzarbrilian@gmail.com>
2016-04-25 20:55:03 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
 						<h2>Forever-a-wanderer. Never-surrender.<span>!</span></h2>
 							<p>Who am I? come find out!</p>
                             <br>
-							 <button><a href="index.php" class="scroll top"><span style="color:#000" class="glyphicon glyphicon-home" aria-hidden="true"> Home</span></a></button>
+							 <button><a href="?page=home" class="scroll top"><span style="color:#000" class="glyphicon glyphicon-home" aria-hidden="true"> Home</span></a></button>
                              <button><a href="index.php#study" class="scroll top"><span style="color:#000" class="glyphicon glyphicon-paperclip" aria-hidden="true"> CV</span></a></button>
                             <button><a href="index.php#contact" class="scroll top"><span style="color:#000" class="glyphicon glyphicon-envelope" aria-hidden="true"> ContactMe</span></a></button>
                            
